383523ba3ab3a3c853e8c40440cfa345def34683
Chuanqi <chuanqi.sun.dev@gmail.com>
2024-09-04 13:45:01 -0700

    simplify sample code

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,17 @@ const credential = new InteractiveBrowserCredential({
   tenantId: "72f988bf-86f1-41af-91ab-2d7cd011db47",
   clientId: "6e4c509e-2b65-40dc-b461-beb2c824f63a",
 });
-const scope = "user.read";
-const azureADTokenProvider = getBearerTokenProvider(credential, scope);
-
-const deployment = "gpt-4o";
-const apiVersion = "2024-07-01-preview";
-const client = new AzureOpenAI({ azureADTokenProvider, deployment, apiVersion, endpoint: "https://proto-api.azure-api.net/" });
+const azureADTokenProvider = getBearerTokenProvider(credential, []);
+const client = new AzureOpenAI({ azureADTokenProvider, apiVersion: "2024-07-01-preview", endpoint: "https://proto-api.azure-api.net/" });
 
 document.querySelector("form")?.addEventListener("submit", async (event) => {
   event.preventDefault();
-  const formData = new FormData(event.target as HTMLFormElement);
-  const userMessage = formData.get("message") as string;
+
+  const content = new FormData(event.target as HTMLFormElement).get("message") as string;
+
   const response = await client.chat.completions
     .create({
-      messages: [{ role: "user", content: userMessage }],
+      messages: [{ role: "user", content }],
       model: "gpt-4o",
     })
     .catch((e) => e);
